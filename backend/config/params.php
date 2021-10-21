e9a22da85b2e15264199821b0444d69ff546d407
SamTheT <ddruganov@bk.ru>
2021-10-21 14:08:24 +0300

    added local params

--- backend/config/params.php
+++ backend/config/params.php
@@ -1,34 +1,41 @@
 <?php
 
-$hosts = [
-    'api' => 'http://localhost:8001',
-    'admin' => 'http://localhost:8080',
-    'client' => 'http://localhost:8005',
-    'service' => 'http://localhost:8007'
-];
+use yii\helpers\ArrayHelper;
+
+$paramsLocalPath = __DIR__ . '/params-local.php';
+$paramsLocal = file_exists($paramsLocalPath) ? require $paramsLocalPath : [];
+
+$finalParams = ArrayHelper::merge([
+    $finalParams['hosts'] = [
+        'api' => 'https://api.linktome.site',
+        'admin' => 'https://admin.linktome.site',
+        'client' => 'https://client.linktome.site',
+        'service' => 'https://service.linktome.site'
+    ]
+], $paramsLocal);
 
 return [
     'token' => [
         'accessTTL' => 1800, // 30 minutes
         'refreshTTL' => 2592000 // 30 days
     ],
-    'hosts' => $hosts,
+    'hosts' => $finalParams['hosts'],
     'links' => [
         'admin' => [
-            'home' => $hosts['admin'],
-            'login' => $hosts['admin'] . '/auth/login',
+            'home' => $finalParams['hosts']['admin'],
+            'login' => $finalParams['hosts']['admin'] . '/auth/login',
             'auth' => [
                 'social' => [
-                    'vk' => $hosts['admin'] . '/auth/social/vk',
-                    'yandex' => $hosts['admin'] . '/auth/social/yandex',
-                    'google' => $hosts['admin'] . '/auth/social/google',
-                    'facebook' => $hosts['admin'] . '/auth/social/facebook',
+                    'vk' => $finalParams['hosts']['admin'] . '/auth/social/vk',
+                    'yandex' => $finalParams['hosts']['admin'] . '/auth/social/yandex',
+                    'google' => $finalParams['hosts']['admin'] . '/auth/social/google',
+                    'facebook' => $finalParams['hosts']['admin'] . '/auth/social/facebook',
                 ]
             ]
         ],
         'service' => [
-            'uploadFolder' => $hosts['service'] . '/upload/image',
-            'defaultImage' => $hosts['service'] . '/images/default.svg',
+            'uploadFolder' => $finalParams['hosts']['service'] . '/upload/image',
+            'defaultImage' => $finalParams['hosts']['service'] . '/images/default.svg',
         ]
     ],
     'socialNetworkApi' => require __DIR__ . '/socialNetworkApi.php',
