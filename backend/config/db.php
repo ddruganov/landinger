b34dbac6e2a02392d255c483e9c8f1f9231a91f1
SamTheT <ddruganov@bk.ru>
2021-10-21 11:54:19 +0300

    db changes

--- backend/config/db.php
+++ backend/config/db.php
@@ -7,8 +7,8 @@ if (file_exists($dbLocalPath)) {
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=linktome',
-    'username' => 'ddruganov',
-    'password' => 'admin',
+    'dsn' => 'pgsql:host=5.45.120.15;dbname=linktome',
+    'username' => 'demid',
+    'password' => 'p3dfJKDHB9HC4x2g',
     'charset' => 'utf8'
 ];
