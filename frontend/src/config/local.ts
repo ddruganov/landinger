a654960ecdf27e80cb66d6811d9f3eb73180593e
SamTheT <ddruganov@bk.ru>
2021-10-21 12:04:59 +0300

    host fix + new build

--- frontend/src/config/local.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    hosts: {
-        api: "http://localhost:8001", // local ? local.host : "https://stt.com",
-        admin: "http://localhost:8080", // local ? local.host : "https://stt.com",
-        client: "http://localhost:8005", // local ? local.host : "https://stt.com",
-        service: "http://localhost:8007", // local ? local.host : "https://stt.com",
-    }
-};
