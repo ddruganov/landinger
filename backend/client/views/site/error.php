8be0c5b48d6f781b74eb13f20ee0a2a2c6e471a6
SamTheT <ddruganov@bk.ru>
2021-10-23 10:39:09 +0300

    client error page fix

--- backend/client/views/site/error.php
+++ backend/client/views/site/error.php
@@ -7,8 +7,10 @@ $exception = Yii::$app->getErrorHandler()->exception;
 <div class="error container">
     <h1>Ошибка!</h1>
     <i class="icon far fa-frown"></i>
-    <h3><?= $exception->getMessage() ?></h3>
-    <?php if (!in_array($exception->getCode(), [404])) : ?>
+    <?php if (in_array($exception->getCode(), [404])) : ?>
+        <h3><?= $exception->getMessage() ?></h3>
+    <?php else : ?>
+        <h3>Ничего страшного!</h3>
         <div>Мы уже знаем об ошибке и скоро всё исправим!</div>
-    <?php endif; ?>
+    <?php endif ?>
 </div>
